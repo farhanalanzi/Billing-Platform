8c10ff3e8af83495374a3589ff18166a5bae13c2
Nawafalzanbaqi <alzanbaqinawaf@gmail.com>
2025-12-01 18:59:59 +0300

    code fix bugs added done

--- bugs.py
+++ bugs.py
@@ -5,3 +5,7 @@ age = int(input("كم عمرك الآن؟ "))
 year_when_100 = (100 - age) + 2025
 
 print(f"مرحباً يا {name}! ستصبح بعمر 100 سنة في سنة {year_when_100}.")
+bug_fixed = True
+if bug_fixed:
+    print("تم إصلاح الخطأ بنجاح!")
+    
